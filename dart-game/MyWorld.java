b7d734a22dfd21d85142c781de633511160649e1
mustaf019 <126239477+mustaf019@users.noreply.github.com>
2024-01-19 15:49:35 +0100

    Pfeile hinzugefügt

--- dart-game/MyWorld.java
+++ dart-game/MyWorld.java
@@ -27,6 +27,6 @@ public class MyWorld extends World
     private void prepare()
     {
         dartboard dartboard = new dartboard();
-        addObject(dartboard,615,191);
+        addObject(dartboard,615,191);       
     }
 }
