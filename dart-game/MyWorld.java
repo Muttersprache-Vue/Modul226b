18b66dd70a89dbed082a1909f2829dbcb7c5ecf0
Muttersprache-Vue <126239128+Muttersprache-Vue@users.noreply.github.com>
2024-01-19 16:35:53 +0100

    Pfeile angeordnet

--- dart-game/MyWorld.java
+++ dart-game/MyWorld.java
@@ -28,5 +28,17 @@ public class MyWorld extends World
     {
         dartboard dartboard = new dartboard();
         addObject(dartboard,615,191);       
+        Versüche versüche = new Versüche();
+        addObject(versüche,1035,475);
+        Versüche versüche2 = new Versüche();
+        addObject(versüche2,1028,463);
+        Versüche versüche3 = new Versüche();
+        addObject(versüche3,1038,488);
+        Versüche versüche4 = new Versüche();
+        addObject(versüche4,1043,499);
+        Versüche versüche5 = new Versüche();
+        addObject(versüche5,1048,510);
+        Versüche versüche6 = new Versüche();
+        addObject(versüche6,1043,511);
     }
 }
